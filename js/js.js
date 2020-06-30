2767f599816668727832e7d91d86fbe5ae61be5b
Viettuan98 <buenotuan98@gmail.com>
2020-06-30 19:08:55 +0700

    upload code

---
+++ js/js.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    function prevSlide() { alert("he"); }
+    //bar su kien change cua the slect
+    $("#lienket").change(function(event) {
+        var url = $("#lienket").val();
+        // di chuyen den url
+        location.href = url;
+    });
+    $('#btn-prev').click(function(event) {
+        /* Act on the event */
+        var slide_truoc = $('.active').prev();
+		if(slide_truoc.length!=0){
+			$('.active').addClass('bien-mat-ben-phai').one('webkitAnimationEnd', function(event) {
+				$('.bien-mat-ben-phai').removeClass('bien-mat-ben-phai').removeClass('active');
+			});
+			slide_truoc.addClass('active').addClass('di-vao-ben-trai').one('webkitAnimationEnd', function(event) {
+				$('.di-vao-ben-trai').removeClass('di-vao-ben-trai');
+			});
+		}else{
+			$('.active').addClass('bien-mat-ben-phai').one('webkitAnimationEnd', function(event) {
+				$('.bien-mat-ben-phai').removeClass('bien-mat-ben-phai').removeClass('active');
+			});
+			$('.slide:last-child').addClass('active').addClass('di-vao-ben-trai').one('webkitAnimationEnd', function(event) {
+				$('.di-vao-ben-trai').removeClass('di-vao-ben-trai');
+			});
+		}
+
+    });
+    $('#btn-next').click(function(event) {
+        /* Act on the event */
+        var slide_sau = $('.active').next();
+        console.log(slide_sau.length);
+        if (slide_sau.length != 0) {
+            $('.active').addClass('bien-mat-ben-trai').one('webkitAnimationEnd', function(event) {
+                $('.bien-mat-ben-trai').removeClass('bien-mat-ben-trai').removeClass('active');
+            });
+            slide_sau.addClass('active').addClass('di-vao-ben-phai').one('webkitAnimationEnd', function(event) {
+                $('.di-vao-ben-phai').removeClass('di-vao-ben-phai');
+            });
+        } else {
+            $('.active').addClass('bien-mat-ben-trai').one('webkitAnimationEnd', function(event) {
+                $('.bien-mat-ben-trai').removeClass('bien-mat-ben-trai').removeClass('active');
+            });
+            $('.slide:first-child').addClass('active').addClass('di-vao-ben-phai').one('webkitAnimationEnd', function(event) {
+                $('.di-vao-ben-phai').removeClass('di-vao-ben-phai');
+            });
+        }
+    });
+});
